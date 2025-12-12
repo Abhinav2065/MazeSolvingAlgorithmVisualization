b5586598de2da50a60edba2a68e69a4e3ecc9e39
Abhinav2065 <abhinavsl511@gmail.com>
2025-12-12 17:56:03 +0545

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,38 @@
+// Core OpenGL Headers
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <cstddef>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <iostream>
+
+
+
+int main() {
+    // Initilize GLFW
+    if (!glfwInit()){
+        std::cerr << "Failed to initilize GLFW\n";
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+    // Create the Window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Starter Project", NULL, NULL);
+    if (window == NULL) {
+        std::cerr << "Failed to Create an GLFW Window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+
+
+
+
+    return 0;
+}
